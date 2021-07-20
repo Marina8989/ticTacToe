47a41224c9c8470eb3d0c69793ead0258354d9d8
Marina8989 <marina.mill209@gmail.com>
2021-07-20 12:01:37 -0700

    tic tac toe project

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
 const box = Array.from(document.querySelectorAll('.box'));
+const spaces = [null, null, null, null, null, null, null, null, null];
+const playText = document.querySelector('#playText');
 const X_Player = 'X';
 const O_Player = 'O';
 let playerTurn = X_Player;
 
-box.forEach((item, index) => {
-    if(index < 3) {
-       item.style.borderBottom = '2px solid green';
-    }
-    if(index >= 6) {
-       item.style.borderTop = '2px solid green';
-    }
-    if(index % 3 == 0) {
-       item.style.borderRight = '2px solid green';
-    }
-    if(index == 2 || index == 5 || index == 8) {
-      item.style.borderLeft = '2px solid green';
-    }
-})
-
-console.log(box);
-
-function startGame() {
-    box.forEach((item, index )=> {
-        item.addEventListener('click', (e)=> {
-            console.log(index);
-            if(index == e.target.id) {
-            item.innerText = playerTurn;
-             playerTurn = playerTurn === X_Player ? O_Player : X_Player;
-            }
-        })
-    })
+const drawBoard = () => {
+    box.forEach((item, index) => {
+      if(index < 3) {
+        item.style.borderBottom = '2px solid green';
+      }
+      if(index >= 6) {
+        item.style.borderTop = '2px solid green';
+      }
+      if(index % 3 == 0) {
+        item.style.borderRight = '2px solid green';
+      }
+      if(index == 2 || index == 5 || index == 8) {
+        item.style.borderLeft = '2px solid green';
+      }
+     item.addEventListener('click', startGame);
+  })
+  
 }
-startGame();
+
+
+const startGame = (e) => {
+  
+  const id = e.target.id;
+      if(!spaces[id]) {
+         spaces[id] = playerTurn;
+         e.target.innerText = playerTurn;
+          //  if(hasThePlayerWOn(playerTurn)) {
+          //    playText.innerText = `Player ${playerTurn} won!`;
+          //    return;
+          //  }
+         playerTurn = playerTurn === X_Player ? O_Player : X_Player;
+      }
+}
+
+drawBoard();
 
